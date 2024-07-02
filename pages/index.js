52c0fb084a1c0d2b83efed5c746c345faac74ffa
saifborghol <saifborghol@gmail.com>
2024-07-02 03:46:43 +0200

    last commit

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,100 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { useRef, useState } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import logger from "../lib/logger";
 
 export default function Home() {
+  const [errorMessage, setErrorMessage] = useState("");
+  const canvasRef = useRef(null);
+  const [screenshot, setScreenshot] = useState(null);
+
+  const captureScreenshot = async () => {
+    if (navigator.mediaDevices) {
+      try {
+        const stream = await navigator.mediaDevices.getDisplayMedia({
+          video: {
+            displaySurface: "monitor",
+          },
+          audio: {
+            suppressLocalAudioPlayback: false,
+          },
+          preferCurrentTab: false,
+          selfBrowserSurface: "exclude",
+          systemAudio: "include",
+          surfaceSwitching: "include",
+          monitorTypeSurfaces: "include",
+        });
+
+        const track = stream.getVideoTracks()[0];
+        const imageCapture = new ImageCapture(track);
+        const bitmap = await imageCapture.grabFrame();
+
+        if (canvasRef.current) {
+          canvasRef.current.width = bitmap.width;
+          canvasRef.current.height = bitmap.height;
+          const context = canvasRef.current.getContext("2d");
+
+          if (context) {
+            context.drawImage(bitmap, 0, 0, bitmap.width, bitmap.height);
+            const image = canvasRef.current.toDataURL("image/png");
+            setScreenshot(image);
+            stopUserMedia(stream);
+          }
+        }
+      } catch (err) {
+        logger.error(err);
+        setErrorMessage("Error getting screenshot. Please allow screen access in your browser settings.");
+      }
+    } else {
+      setErrorMessage("navigator.mediaDevices not available in this browser");
+    }
+  };
+
+  const stopUserMedia = (stream) => {
+    stream.getTracks().forEach((track) => {
+      track.stop();
+    });
+  };
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Share Your Screenshot</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className={styles.content}>
+        <h1 className={styles.title}>Select your preferred screen!</h1>
+        <button className={styles.card} onClick={captureScreenshot}>
+          <p>Allow the browser to access screenshot</p>
+        </button>
       </main>
-
-      <footer>
+      <div className="image-container">
+        {screenshot && (
+          <img
+            src={screenshot}
+            alt="Screenshot"
+            className={styles.screenshotStyle}
+          />
+        )}
+      </div>
+      <div className="canvas-container">
+        <canvas ref={canvasRef} style={{ display: "none" }} />
+      </div>
+      <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.officent.co/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+          Powered by{" "}
+          <img
+            src="/officent_IT_logo.png"
+            alt="Officent"
+            className={styles.logo}
+          />
         </a>
       </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
